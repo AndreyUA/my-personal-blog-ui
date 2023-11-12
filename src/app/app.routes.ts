407b20b715ca6e14c74880ffb3f73443f9008773
AndreyUA <infernalsteel666@gmail.com>
2023-11-12 11:30:28 +0200

    created home page and configured routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./feature/home/home.routes').then((m) => m.routes),
+  },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' },
+];

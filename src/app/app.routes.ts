0b8323003aec2ededb3df91f11b3ea1ac036a40d
AndreyUA <infernalsteel666@gmail.com>
2023-11-12 11:48:32 +0200

    creted leyout component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,19 @@
 import { Routes } from '@angular/router';
 
+import { LayoutComponent } from './shared/layout/layout.component';
+
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
-    path: 'home',
-    loadChildren: () =>
-      import('./feature/home/home.routes').then((m) => m.routes),
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('./feature/home/home.routes').then((m) => m.routes),
+      },
+    ],
   },
   { path: '**', redirectTo: '/home', pathMatch: 'full' },
 ];

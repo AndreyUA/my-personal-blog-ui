dc945dd342a919c328e1d56c06eff2f47187e58f
AndreyUA <infernalsteel666@gmail.com>
2023-11-14 12:36:48 +0200

    implemented header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { LanguageSwitchComponent } from '../components/language-switch/language-switch.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, LanguageSwitchComponent],
+  imports: [
+    CommonModule,
+    LanguageSwitchComponent,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
